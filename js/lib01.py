c7f42399d5517f200d2d3ddbefc6dcffac441c5f
Mikhail Lapin <mlapin@rambler.ru>
2019-10-07 09:16:47 +0300

    xxx

--- js/lib01.py
+++ js/lib01.py
@@ -1,5 +1,88 @@
 import codecs
 
+string1jqh = '''\
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<base target="_blank">
+'''
+
+string3jqh = '''	
+	<link rel="stylesheet" href="css/style.css" type="text/css" />  
+	<script src="lib/jquery.js" type="text/javascript"></script>
+<script>
+
+</script>
+</head>
+<body>
+<script>
+		sss111 = ''
+		function print1(s1, s2='',s3='',s4 ='', s5='<br>'){sss111 += s1+' '+s2+' '+s3+' '+s4+' '+s5}
+</script>
+
+'''
+string5jqh = '''\
+    <div id="d_78">____</div>
+    <script>
+		var el = document.getElementById('d_78')
+		el.innerHTML = sss111
+	</script>
+
+</body>
+</html>
+'''
+
+def writeJQH(name, string2, string1 = ''):
+    global string1jqh, string3jqh, string5jqh
+    with codecs.open(name, 'w', 'utf-8') as file:
+        file.write(string1jqh + string1 + string3jqh + string2 + string5jqh)
+
+string0jq = '''\
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<base target="_blank">
+	<style>
+		.myList{
+			color:green;
+		}
+	</style>
+	<link rel="stylesheet" href="css/style.css" type="text/css" />  
+	<script src="lib/jquery.js" type="text/javascript"></script>
+<script>
+
+</script>
+</head>
+<body>
+<script>
+		sss111 = ''
+		function print1(s1, s2='',s3='',s4 ='', s5='<br>'){sss111 += s1+' '+s2+' '+s3+' '+s4+' '+s5}
+</script>
+
+'''
+string2jq = '''\
+    <div id="d_78">____</div>
+    <script>
+		var el = document.getElementById('d_78')
+		el.innerHTML = sss111
+	</script>
+
+</body>
+</html>
+'''
+
+def writeJQ(name, string1):
+    global string0jq, string2jq
+    with codecs.open(name, 'w', 'utf-8') as file:
+        file.write(string0jq + string1 + string2jq)
+
+# writeJQ("1.txt", "33333")
+
+
 string0 = '''\
 <!DOCTYPE html>
 <html lang="en">
